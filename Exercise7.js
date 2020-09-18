af739247d1606923263aec6dcd0fb7ddc3e31b18
Gabriel <anjos.gabriel999@gmail.com>
2020-09-18 19:28:35 -0300

    Exercise7

---
+++ Exercise7.js
@@ -0,0 +1,5 @@
+const number = 2.8;
+const double = number * 2;
+const thirdPart = number / 3;
+console.log(`The double of ${number}, it's ${double}`)
+console.log(`The third part of ${number}, it's ${thirdPart}`)
