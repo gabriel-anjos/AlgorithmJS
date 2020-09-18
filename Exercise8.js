ff72e738669d9a2d330991227f5c8611c2056901
Gabriel <anjos.gabriel999@gmail.com>
2020-09-18 19:58:58 -0300

    Exercise8

---
+++ Exercise8.js
@@ -0,0 +1,15 @@
+const distance = 185.72;
+const km = distance / 10 ** 3;
+const hm = distance / 10 ** 2;
+const dam = distance / 10 ** 1;
+const dm = distance * 10 ** 1;
+const cm = distance * 10 ** 2;
+const mm = distance * 10 ** 3;
+console.log(`Type the distance in meters: ${distance}`);
+console.log(`The distance of ${distance} correspond: `);
+console.log(`${km}Km`)
+console.log(`${hm}hm`)
+console.log(`${dam}dam`)
+console.log(`${dm}dm`)
+console.log(`${cm}cm`)
+console.log(`${mm}mm`)
