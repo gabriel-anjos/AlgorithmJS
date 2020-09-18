5e9cd2ec28e9cc732e12304becbf90f5331d7b5f
Gabriel <anjos.gabriel999@gmail.com>
2020-09-18 18:31:48 -0300

    Exercise3

---
+++ Exercise3.js
@@ -0,0 +1,6 @@
+const name = "Gabriel";
+const payment = 1850.50;
+console.log(`Name of the Employee: ${name}`)
+console.log(`Payment: ${payment}`)
+console.log(`The employee ${name},has a payment of US$${payment}`)
+
