dbebbc293c7561cbf3db725c691366b3a3ec9c43
Gabriel <anjos.gabriel999@gmail.com>
2020-09-18 20:28:24 -0300

    Exercise10

---
+++ Exercise10.js
@@ -0,0 +1,7 @@
+const width = 8;
+const height = 9;
+const area = width * height;
+const ink = 2 ** 2;
+const inkTotal = area / ink;
+console.log(`The area of wall it's ${area} square maters. `)
+console.log(`The wall will require ${inkTotal} liters of ink.`)
