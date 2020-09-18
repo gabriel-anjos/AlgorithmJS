0e506b8167e13175b601189768c474f496c520ec
Gabriel <anjos.gabriel999@gmail.com>
2020-09-18 20:44:39 -0300

    Exercise12

---
+++ Exercise12.js
@@ -0,0 +1,5 @@
+const price = 8;
+const discount = 5 * price / 100;
+const discountPrice = price - discount;
+console.log(`Product price: ${price}.`)
+console.log(`Product with 5% off: ${discountPrice}.`)
