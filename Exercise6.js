cf8f947b36ffbe92c04a7098788b97a9ab9698d5
Gabriel <anjos.gabriel999@gmail.com>
2020-09-18 19:13:48 -0300

    Exercise6

---
+++ Exercise6.js
@@ -0,0 +1,5 @@
+const number1 = 8;
+const predecessor = number1 - 1;
+const successor = number1 + 1;
+console.log(`The predecessor of ${number1} it's ${predecessor} `)
+console.log(`The successor of ${number1} it's ${successor}`)
