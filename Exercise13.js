49144f4fbceed7e36aa09533c608e146afcdf16c
Gabriel <anjos.gabriel999@gmail.com>
2020-09-18 20:52:54 -0300

    Exercise13

---
+++ Exercise13.js
@@ -0,0 +1,5 @@
+const payment = 1250;
+const paymentIncrease = 15 * payment / 100;
+const newPayment = payment + paymentIncrease;
+console.log(`Current payment: ${payment}.`);
+console.log(`Payment with increase ajust: ${newPayment}.`)
