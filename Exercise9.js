19ff2ae4961771838bd052c82db3942877039358
Gabriel <anjos.gabriel999@gmail.com>
2020-09-18 20:13:38 -0300

    Exercise9

---
+++ Exercise9.js
@@ -0,0 +1,5 @@
+const wallet = 88;
+const dollar = 3.45;
+const conversion = wallet / dollar;
+console.log(`Your balence it's of R$ ${wallet}`);
+console.log(`The balance after de conversion it's Us$ ${conversion}`);
