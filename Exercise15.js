186c24a6bc40d23319f141ee4ebc3ad8d5f9f4ba
Gabriel <anjos.gabriel999@gmail.com>
2020-09-18 21:24:26 -0300

    Exercise15

---
+++ Exercise15.js
@@ -0,0 +1,7 @@
+const workDays = 30;
+const hourPay = 20;
+const workHour = 8;
+const payment = (workHour * hourPay) * workDays;
+console.log(`Work days: ${workDays}.`)
+console.log(`Work hour per day: ${workHour}.`)
+console.log(`The month payment it's: R$ ${payment}.`)
