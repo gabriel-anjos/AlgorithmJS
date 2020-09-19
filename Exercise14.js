28d7defc739af78d6ea088162422a299e9914c24
Gabriel <anjos.gabriel999@gmail.com>
2020-09-18 21:11:10 -0300

    Exercise14

---
+++ Exercise14.js
@@ -0,0 +1,8 @@
+const distance = 85;
+const days = 5;
+const costDistance = distance * 0.20;
+const costDays = days * 90;
+const finalCost = costDistance + costDays;
+console.log(`Your total distance traveled was : ${distance} Km. `)
+console.log(`Your total days with the car was : ${days} days.`)
+console.log(`The final cost for the rent of the car it's : R$ ${finalCost} `)
