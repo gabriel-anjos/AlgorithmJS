4e6c232865f8807099b8dea66b60ae2b9814a209
Gabriel <anjos.gabriel999@gmail.com>
2020-09-18 21:59:07 -0300

    Exercise16

---
+++ Exercise16.js
@@ -0,0 +1,5 @@
+const CigarettesDay = 12;
+const years = 2;
+const timeDays = (365 * years * CigarettesDay) * 10;
+const timeWaste = timeDays / 1440;
+console.log(`You woste ${timeWaste} days of your life smoking `);
