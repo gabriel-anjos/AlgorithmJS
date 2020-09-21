84c744cdcb2cdba5aed5a788f0242525803a0e6c
Gabriel <anjos.gabriel999@gmail.com>
2020-09-21 18:03:37 -0300

    Exercise26

---
+++ Exercise26.js
@@ -0,0 +1,12 @@
+const number1 = 8;
+const number2 = 3;
+
+if (number1 == number2) {
+    console.log(`The values are equals!`)
+} else {
+    if (number1 < number2) {
+        console.log(`The seconde value it's bigger.`)
+    } else {
+        console.log(`The first value it's bigger.`)
+    }
+}
